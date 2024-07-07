f21a2e39d810c805a6140287bf521416d3789619
Muhammad Zain Ur Rehman <32788650+zain505@users.noreply.github.com>
2024-07-07 22:48:09 +0500

    verified auth route
    verified auth

--- routes/candidate.js
+++ routes/candidate.js
@@ -3,10 +3,12 @@ const { check } = require('express-validator');
 
 const candidController = require('../controllers.js/candidate_controller');
 
+const verifyToken = require('../authMiddleWare/authMiddleWare');
+
 const router = express.Router();
 
-router.get('/',candidController.getAllRegisteredCandidates)
+router.get('/', verifyToken, candidController.getAllRegisteredCandidates)
 
-router.post('/create-candidate',candidController.createCandidate)
+router.post('/create-candidate', verifyToken, candidController.createCandidate)
 
 module.exports = router;
