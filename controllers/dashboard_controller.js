5d21464009ef347fe597cd1cc6ef16feb8fbdc29
Muhammad Zain Ur Rehman <32788650+zain505@users.noreply.github.com>
2024-08-20 23:00:43 +0500

    change

--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -12,33 +12,36 @@ const getAllMasterInfo = async (req, res, next) => {
 
     // user master info
 
-    const totalActiveusers = totalUsersFromDb.filter(u=>u.isActive).length;
+    const totalActiveusers = totalUsersFromDb.filter(u => u.isActive).length;
 
-    const totalInActiveusers = totalUsersFromDb.filter(u=>u.isActive==false).length;
+    const totalInActiveusers = totalUsersFromDb.filter(u => u.isActive == false).length;
 
     const totalUsers = totalUsersFromDb.length;
 
 
     //candidate master info
 
-    const totalActiveCandids = totalCandidatesFromDb.filter(c=>c.is_candidate_approved_by_admin_for_interview).length;
+    const totalAdminApprovedCandidsForInterviews = totalCandidatesFromDb.filter(c => c.is_candidate_approved_by_admin_for_interview).length;
 
-    const totalInActiveCandids = totalCandidatesFromDb.filter(c=>c.is_candidate_approved_by_admin_for_interview==false).length;
+    const totalInActiveCandids = totalCandidatesFromDb.filter(c => c.is_candidate_approved_by_admin_for_interview == false).length;
+
+    const todayCreatedCandids = totalCandidatesFromDb.filter(c => new Date(c.creationDate.toString()) == new Date()).length;
 
     const totalCandids = totalCandidatesFromDb.length;
 
     //employees master info;
 
-    const totalActiveEmployees = totalEmployeesFromDB.filter(emp=>emp.isEmployee);
+    const totalActiveEmployees = totalEmployeesFromDB.filter(emp => emp.isEmployee).length;
 
     res.status(200).json({
         totalActiveusers,
         totalUsers,
-        totalActiveCandids,
+        totalAdminApprovedCandidsForInterviews,
         totalCandids,
         totalInActiveusers,
         totalInActiveCandids,
-        totalActiveEmployees
+        totalActiveEmployees,
+        todayCreatedCandids
     })
 
 
